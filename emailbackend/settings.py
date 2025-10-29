a001fe1b36169daf3908dca61aed0be585001daa
Glory <ayanwolaglory@gmail.com>
2025-10-29 11:02:19 +0100

    add static file url to settings py

--- emailbackend/settings.py
+++ emailbackend/settings.py
@@ -40,6 +40,13 @@ CORS_ALLOWED_ORIGINS = [
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",") + ["*"]
 
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.2/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 # ALLOWED_HOSTS = ['*']
@@ -188,12 +195,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
